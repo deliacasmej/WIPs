70cdaaf0eebb96b14c0da5ab28f103a1a0f94b0c
Delia <100162201+deliacasmej@users.noreply.github.com>
2025-02-08 19:30:49 -0600

    Update universeFCC.sql

--- universeFCC.sql
+++ universeFCC.sql
@@ -90,3 +90,5 @@ universe=> ALTER TABLE star ADD COLUMN age_in_millions_of_years INT;
 ALTER TABLE
 universe=> ALTER TABLE galaxy ADD COLUMN galaxy_table VARCHAR(30) NOT NULL;
 ALTER TABLE
+universe=> ALTER TABLE galaxy ADD COLUMN info_galaxy SERIAL;
+ALTER TABLE
